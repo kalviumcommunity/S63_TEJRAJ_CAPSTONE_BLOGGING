63108d2ac556a2286e036e783186aba61ef75d8d
Tejraj <vishnu9929059003@gmail.com>
2025-04-17 11:36:50 +0530

    Put_Api

---
+++ blog-backend/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.use(express.json()); // Add this line to parse JSON bodies
+
+let blogs = [
+    {id: 1, title: 'First Blog', content: 'Hello World'},
+    {id: 2, title: "Second Blog", content: "This is another post"}
+];
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Blog API!');
+});
+
+app.get('/api/blogs', (req, res) => {
+    res.json(blogs);
+});
+
+app.post('/api/blogs/post', (req, res) => {
+    const { title, content } = req.body;
+
+    if (!title || !content) {
+        return res.status(400).json({ message: 'Title and Content are required' });
+    }
+
+    const newBlog = {
+        id: blogs.length + 1,
+        title, content
+    };
+    blogs.push(newBlog);
+    res.status(201).json(newBlog);
+});
+
+app.put('/api/blogs/update', (req, res) => {
+    const { id, title, content } = req.body;
+
+    if (!id || !title || !content) {
+        return res.status(400).json({ message: 'ID, Title and Content are required.' });
+    }
+
+    const blogIndex = blogs.findIndex(blog => blog.id === parseInt(id));
+
+    if (blogIndex === -1) {
+        return res.status(404).json({ message: 'Blog not found.' });
+    }
+
+    blogs[blogIndex] = { ...blogs[blogIndex], title, content };
+
+    res.status(200).json({
+        message: `Blog with ID ${id} updated successfully.`,
+        updatedBlog: blogs[blogIndex]
+    });
+});
+
+
+app.listen(5000, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
